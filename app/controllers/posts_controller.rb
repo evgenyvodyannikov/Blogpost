66ac04ba80ece110b628ff32c44237d5e16a19d8
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-05-01 20:34:37 +0500

    create new post controller and route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
     def index
         
     end
+
+    def new 
+        posts_path = 'ss'
+    end
+
+    def create 
+        render plain: params[:post].inspect
+    end 
 end
